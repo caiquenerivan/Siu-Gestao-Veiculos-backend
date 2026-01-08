6160249e5df0b54e4fdb3afdba66252871279156
Caique Nerivan <caiquenerivansantos@gmail.com>
2026-01-07 21:54:42 -0300

    correção do package json

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
